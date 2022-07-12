85b92b8abeb786ff9e60db835289b4014765b357
Kristijan Baro <baro.kristijan@gmail.com>
2022-07-12 14:59:15 +0200

    adding css and uploading movies form API

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 
+const main = document.querySelector('main');
+
 async function getMovies() {
     const resp = await fetch(APIURL);
     const respData = await resp.json();
@@ -9,10 +11,21 @@ async function getMovies() {
     console.log(respData);
 
     respData.results.forEach((movie) => {
-        const img = document.createElement('img');
-        img.src = IMGPATH + movie.poster_path;
+        const { poster_path, title, vote_average } = movie;
 
-        document.body.appendChild(img);
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `<img src="${IMGPATH + poster_path}"
+                                alt="${title}"
+                                    />
+                            <div class="movie-info">
+                                <h3>${title}</h3>
+                                <span>${vote_average}</span>
+                            </div>
+                        `;
+
+                        document.body.appendChild(movieEl);
     });
 
     return respData;
