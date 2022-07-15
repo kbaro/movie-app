de8dc220f3b1ad47e6ac5d99dc3384395df7b67a
Kristijan Baro <baro.kristijan@gmail.com>
2022-07-15 13:38:30 +0200

    Movie App with searh bar and ratings

--- script.js
+++ script.js
@@ -1,21 +1,30 @@
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const filter = document.getElementById('filter');
 
-const main = document.querySelector('main');
-const form = document.querySelector('form');
-const search = document.querySelector('search');
+// initially get favorite movies
+getMovies(APIURL);
 
-getMovies();
-
-async function getMovies() {
-    const resp = await fetch(APIURL);
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
     console.log(respData);
 
-    respData.results.forEach((movie) => {
-        const { poster_path, title, vote_average } = movie;
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+    //clear main
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
 
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
@@ -28,12 +37,15 @@ async function getMovies() {
                                 <h3>${title}</h3>
                                 <span class="${getClassByRate(vote_average)}">${vote_average}</span>
                             </div>
+                            <div class="overview">
+                                <h3>Overview:</h3>
+                                ${overview}
+                            </div>
                         `;
 
-                        main.appendChild(movieEl);
+                    main.appendChild(movieEl);
     });
 
-    return respData;
 }
 
 function getClassByRate(vote) {
@@ -50,6 +62,13 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const searchTerm = search.value;
-})
+
+    if(searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
+
 
 
